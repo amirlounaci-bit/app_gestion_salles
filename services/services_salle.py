d3a5fec63a7395da27232b7dee2d06e8578902c3
unknown <amirlounaci@gmail.com>
2026-04-19 13:42:12 -0400

    Ajout de la méthode rechercher_salle dans ServiceSalle

--- services/services_salle.py
+++ services/services_salle.py
@@ -23,6 +23,14 @@ class ServiceSalle:
             return False , "la capacite doit etre superieur ou egale a 1"
         self.dao_salle.update_salle(salle)
         return True,("salle modifie avec succes")
+
     def supprimer_salle(self, code):
         self.dao_salle.delete_salle(code)
         return True, "salle suprimee avec succes"
+
+    def rechercher_salle(self, code):
+        resultat=self.dao_salle.get_salle(code)
+        if resultat:
+            return salle (resultat[0], resultat[1], resultat[2], resultat[3])
+        else:
+            return None
