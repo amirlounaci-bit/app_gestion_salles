e286333eafd41bb0878bb3ef4957ecb966ea36a0
unknown <amirlounaci@gmail.com>
2026-04-19 13:26:45 -0400

    Implémentation de la méthode modifier_salle dans ServiceSalle avec validation des données

--- services/services_salle.py
+++ services/services_salle.py
@@ -12,6 +12,14 @@ class ServiceSalle:
         if salle.capacite < 1:
             return False , "la capacite doit etre superieur ou egale a 1"
 
-        self.dao.sales.insert(salle)
+        self.dao_salle.insert_salle(salle)
 
         return True,"salle ajoutee avec succes"
+
+    def modifer_sale(self, salle):
+        if salle.code=="" or salle.description==""or salle.categorie=="" or salle.capacite=="":
+            return False ,"veuillez remplir tout les champs"
+        if salle.capacite < 1:
+            return False , "la capacite doit etre superieur ou egale a 1"
+        self.dao_salle.update_salle(salle)
+        return True,("salle modifie avec succes")
