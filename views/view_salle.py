c74641abce20036bd2abb6713f5271d914d18149
unknown <amirlounaci@gmail.com>
2026-04-19 19:53:08 -0400

    creation de la class viewsalle

---
+++ views/view_salle.py
@@ -0,0 +1,18 @@
+import customtkinter as ctk
+from services.services_salle import ServiceSalle
+from models.salle import Salle
+
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+
+
+
+
+
+
+
+
+
+
