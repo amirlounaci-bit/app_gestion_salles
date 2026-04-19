287c50801dc59f38cab1dcafdc142e7bd0a39e91
unknown <amirlounaci@gmail.com>
2026-04-19 13:18:32 -0400

    Implémentation de la méthode ajouter_salle dans ServiceSalle avec validation des données

--- services/services_salle.py
+++ services/services_salle.py
@@ -4,3 +4,14 @@ from data.dao_salle import DataSalle
 class ServiceSalle:
     def __init__(self):
         self.dao = DataSalle()
+
+    def ajouter_sale(self, salle):
+        if salle.code==""or salle.description=="" or salle.categorie==""or salle.capacite=="":
+            return False ,"veuillez remplir tout les champs"
+
+        if salle.capacite < 1:
+            return False , "la capacite doit etre superieur ou egale a 1"
+
+        self.dao.sales.insert(salle)
+
+        return True,"salle ajoutee avec succes"
