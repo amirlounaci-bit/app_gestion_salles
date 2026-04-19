663627fe072f98f7a195e8193b386437ea8c4f8b
unknown <amirlounaci@gmail.com>
2026-04-19 13:30:55 -0400

    Implémentation de la méthode suprimer_salle dans ServiceSalle avec validation des données

--- services/services_salle.py
+++ services/services_salle.py
@@ -5,7 +5,7 @@ class ServiceSalle:
     def __init__(self):
         self.dao = DataSalle()
 
-    def ajouter_sale(self, salle):
+    def ajouter_salle(self, salle):
         if salle.code==""or salle.description=="" or salle.categorie==""or salle.capacite=="":
             return False ,"veuillez remplir tout les champs"
 
@@ -16,10 +16,13 @@ class ServiceSalle:
 
         return True,"salle ajoutee avec succes"
 
-    def modifer_sale(self, salle):
+    def modifer_salle(self, salle):
         if salle.code=="" or salle.description==""or salle.categorie=="" or salle.capacite=="":
             return False ,"veuillez remplir tout les champs"
         if salle.capacite < 1:
             return False , "la capacite doit etre superieur ou egale a 1"
         self.dao_salle.update_salle(salle)
         return True,("salle modifie avec succes")
+    def supprimer_salle(self, code):
+        self.dao_salle.delete_salle(code)
+        return True, "salle suprimee avec succes"
